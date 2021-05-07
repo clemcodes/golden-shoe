7cded23ab02c843e6f4b4d95b990e35057cc8b49
clemcodes <clemcodes@outlook.com>
2021-05-07 19:18:54 +0100

    fech data from react - useEffect

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, Card, Button, Accordion, Table } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
 
-  // return the right shoe, get the url id with props.match.params 
-  const product = products.find(p => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      // get the url id with props.match.params 
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
 
   return (
     <>
