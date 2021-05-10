7853dfbd08a505a87322631d6afa7d2f15e84406
clemcodes <clemcodes@outlook.com>
2021-05-10 11:59:39 +0100

    reformating

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
@@ -23,18 +23,22 @@ const Footer = () => {
           <Col className='text-center py-3' sm={12} md={4}>
             <h6 className='pb-3'>Contact Us</h6>
             <p>Mon-Sat 9AM - 5PM</p>
-            <p><i className="fas fa-phone-alt"></i> +44 0123 4567 8900</p>
-            <p><i className="fas fa-envelope"></i> support@goldenshoe.com</p>
+            <p>
+              <i className='fas fa-phone-alt'></i> +44 0123 4567 8900
+            </p>
+            <p>
+              <i className='fas fa-envelope'></i> support@goldenshoe.com
+            </p>
             <p className='social-panel'>
-              <i className="fab fa-twitter mr-3"></i>
-              <i className="fab fa-facebook mr-3"></i>
-              <i className="fab fa-instagram"></i>
+              <i className='fab fa-twitter mr-3'></i>
+              <i className='fab fa-facebook mr-3'></i>
+              <i className='fab fa-instagram'></i>
             </p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
