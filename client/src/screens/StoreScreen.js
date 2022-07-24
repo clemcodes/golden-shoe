3062dabebeff3545190b6dd821e083fc869d7e53
clemcodes <clemcodes@outlook.com>
2022-07-24 18:09:14 +0100

    add Message and Loader Components

--- client/src/screens/StoreScreen.js
+++ client/src/screens/StoreScreen.js
@@ -3,6 +3,8 @@ import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../actions/productActions';
 import Product from '../components/Product';
 import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 
 const StoreScreen = () => {
@@ -18,14 +20,19 @@ const StoreScreen = () => {
 
   return (
     <>
-      {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> :
+      {loading ? (
+        <Loader /> 
+      ): error ? (
+        <Message variant='danger'>{error}</Message>
+      ): (
         <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
-      </Row>
+        </Row>
+        )
       } 
     </>
   );
